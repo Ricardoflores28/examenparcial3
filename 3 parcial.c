364ee964a8aef08d5f844b2841198b35d47bc297
Ricardoflores28 <48325337+Ricardoflores28@users.noreply.github.com>
2019-06-11 14:20:11 -0500

    examen tercera parcial
    se agrego la bienvenida "Hola Mundo"

--- 3 parcial.c
+++ 3 parcial.c
@@ -1,17 +1,19 @@
-#include<include/18F4620.h>
-#include<stdio.h>
-#include<stdlib.h>
+
+#include <include/18F4620.h>
 #fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=40000000)
-#define __DEBUG_SERIAL__
-#ifdef __DEBUG_SERIAL__
-   #define TX_232        PIN_D6
-   #use RS232(BAUD=9600, XMIT=TX_232, BITS=8,PARITY=N, STOP=1)
-#endif
+//#include <stdio.h>
+//#include <stdlib.h>
+#define __DEBUG_SERIAL__ //Si comentas esta linea se deshabilita el debug por serial y el PIN_C6 puede ser usado en forma digital I/O
 
-int main(int argc, char argv)
-{
+#ifdef __DEBUG_SERIAL__
+   #define TX_232        PIN_C6
+   #use RS232(BAUD=9600, XMIT=TX_232, BITS=8,PARITY=N, STOP=1)
+   #use fast_io(c)
+#endif
+int main(int argc, char argv){
+    printf("Hola mundo!\n");
     TRISB = 0;
     int i;
     
